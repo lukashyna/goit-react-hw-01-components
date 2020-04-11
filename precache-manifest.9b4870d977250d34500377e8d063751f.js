b8dba990eba1770ca64a1c1145caab3ae9c2dbea
Anastasia Lukashyna <L_A_A_93@mail.ru>
2020-04-11 14:03:40 +0300

    Updates

---
+++ precache-manifest.9b4870d977250d34500377e8d063751f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b1321cadd597e336237095b337fa98e",
+    "url": "/goit-react-hw-01-components/index.html"
+  },
+  {
+    "revision": "aa5f861edf1c3c6d4f70",
+    "url": "/goit-react-hw-01-components/static/css/main.7b5070db.chunk.css"
+  },
+  {
+    "revision": "73a464ac2c7b46795d26",
+    "url": "/goit-react-hw-01-components/static/js/2.da57727b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/goit-react-hw-01-components/static/js/2.da57727b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aa5f861edf1c3c6d4f70",
+    "url": "/goit-react-hw-01-components/static/js/main.a01070bb.chunk.js"
+  },
+  {
+    "revision": "f2ebcb926d6c2cbee00b",
+    "url": "/goit-react-hw-01-components/static/js/runtime-main.62340070.js"
+  }
+]);
